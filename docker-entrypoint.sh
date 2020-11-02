25d2611b060f7a53f19d4e5bad42a2fb2c4a7a80
TapTap21 <wernich.bekker@gmail.com>
2020-11-02 16:18:27 +0200

    FIX add ports to ssh-keyscan

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -59,16 +59,14 @@ ssh-add ~/.ssh/id_rsa
 
 
 echo "Add known hosts"
-#printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
-ssh-keyscan "$SSH_HOST" >> ~/.ssh/known_hosts
-ssh-keyscan "$SSH_HOST" >> /etc/ssh/ssh_known_hosts
+ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> ~/.ssh/known_hosts
+ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> /etc/ssh/ssh_known_hosts
 # set context
 echo "Create docker context"
 docker context create staging --docker "host=ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
 docker context use staging
 
 
-docker ps
 if  [ -n "$INPUT_DOCKER_LOGIN_PASSWORD" ] || [ -n "$INPUT_DOCKER_LOGIN_USER" ] || [ -n "$INPUT_DOCKER_LOGIN_REGISTRY" ]; then
   echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: docker login"
   docker login -u "$INPUT_DOCKER_LOGIN_USER" -p "$INPUT_DOCKER_LOGIN_PASSWORD" "$INPUT_DOCKER_LOGIN_REGISTRY"
